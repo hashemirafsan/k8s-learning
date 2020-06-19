68e665137b3208743b02e94269bc1b5d7de3528b
Hashemi Rafsan <rafsanhashemi@gmail.com>
2020-06-19 12:58:29 +0600

    update practice

--- node/index.js
+++ node/index.js
@@ -3,5 +3,6 @@ const server = require('http').Server(app);
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello world'))
+app.get('/v2', (req, res) => res.send('Hello world v2'))
 
 server.listen(PORT, () => console.log('Server listening at port %d', PORT) )
